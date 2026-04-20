7ebf8e29e528a7081662862ef6075f46a30a55d6
Wesley Ryan <wesley.ryan03@gmail.com>
2026-04-20 20:05:37 -0300

    refactor: split css by page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
 import App from './App.jsx'
 import TransactionsPage from './pages/TransactionsPage.jsx'
+
+import './styles/reset.css'
+import './styles/shared.css'
+import './styles/main-page.css'
+import './styles/transactions-page.css'
+import './styles/responsive.css'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
